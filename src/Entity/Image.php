2523fd2403b41d556df6c87832fbae6998e99ac3
l0ic68 <maxiloic@gmail.com>
2019-06-13 16:26:32 +0200

    provisional Save

---
+++ src/Entity/Image.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Entity\File as EmbeddedFile;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ImageRepository")
+ */
+class Image
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     *
+     * @Assert\NotBlank(message="Please, upload the product brochure as a PDF file.")
+     * @Assert\File(mimeTypes={ "image/*" })
+     */
+    private $filename;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $path;
+
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $alt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Projects", inversedBy="galery")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $project;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFilename()
+    {
+        return $this->filename;
+    }
+
+    public function setFilename($filename)
+    {
+        $this->filename = $filename;
+
+        return $this;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(string $path): self
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    public function getAlt(): ?string
+    {
+        return $this->alt;
+    }
+
+    public function setAlt(string $alt): self
+    {
+        $this->alt = $alt;
+
+        return $this;
+    }
+
+    public function getProject(): ?Projects
+    {
+        return $this->project;
+    }
+
+    public function setProject(?Projects $project): self
+    {
+        $this->project = $project;
+
+        return $this;
+    }
+}
---
+++ src/EventListener/ImageUploadListener.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\File\File;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use App\Entity\Image;
+use App\Service\FileUploader;
+
+class ImageUploadListener
+{
+    private $uploader;
+
+    public function __construct(FileUploader $uploader)
+    {
+        $this->uploader = $uploader;
+    }
+
+    public function prePersist(LifecycleEventArgs $args)
+    {
+     
+        $entity = $args->getEntity();
+
+ 
+       
+        if (!$entity instanceof Image) {
+            var_dump("Not Image");
+            // if(method_exists($entity,'getImage'))
+            // {
+            //     $entity = $entity->getImage();
+            //     $this->uploadFile($entity);
+            // }
+            if(method_exists($entity,'getBanner'))
+            {
+                var_dump("getBanner");
+                $entity = $entity->getBanner();
+                
+                $this->uploadFile($entity);
+            }
+
+            // if(method_exists($entity,'getThumbnail'))
+            // {
+            //     $entity = $entity->getThumbnail();
+            //     $this->uploadFile($entity);
+            // }
+
+            // if(method_exists($entity,'getGalery'))
+            // {
+            //     $entity = $entity->getGalery();
+            //     $this->uploadFile($entity);
+            // }
+        }
+        else{
+            var_dump("image");
+            $this->uploadFile($entity);
+        }
+    }
+
+    public function preUpdate(PreUpdateEventArgs $args)
+    {
+        var_dump("update ?");
+        s;
+        $entity = $args->getEntity();
+
+         if (!$entity instanceof Image) {
+
+            if(method_exists($entity,'getImage'))
+                $entity = $entity->getImage();
+        }
+
+        $this->uploadFile($entity);
+    }
+
+    private function uploadFile($entity)
+    {
+        
+        if(!$entity instanceof Image)
+        {
+            var_dump($entity);
+            s;
+            return;
+        }
+        $file = $entity->getFilename();    
+
+        if ($file instanceof UploadedFile) {
+            $fileName = $this->uploader->upload($file);
+            // $entity->setPath($this->uploader->getTargetDirectory() + $fileName);
+            // var_dump($this->uploader->getTargetDirectory());
+            $entity->setFilename($fileName);
+        } elseif ($file instanceof File) {
+            // prevents the full file path being saved on updates
+            // as the path is    on the postLoad listener
+            $entity->setFilename($file->getFilename());
+            
+        }
+        
+        if ($fileName = $entity->getFilename()) {
+            $entity->setPath("uploads/Thumbnail/".$fileName);
+        }
+        
+    }
+    // public function postLoad(LifecycleEventArgs $args)
+    // {
+    //     $entity = $args->getEntity();
+
+    //     if (!$entity instanceof Product) {
+    //         return;
+    //     }
+
+    //     if ($fileName = $entity->getFilename()) {
+    //         $entity->setPath(new File($this->uploader->getTargetDirectory().'/'.$fileName));
+    //     }
+    // }
+}
